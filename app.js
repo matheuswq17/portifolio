9dfcb0fb2030cf44aca9afc43b32e4b3cef35a21
matheus <matheusxavi57@gmail.com>
2024-02-02 12:15:05 -0300

    Adicionando app.js ao documento

---
+++ app.js
@@ -0,0 +1,13 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbanil');
+
+nextDom.onclick = function(){
+    showSlider('next');
+}
+function showSlider (typer){
+    let itemSlider = docuemnt.querySelector('.carousel .list .item')
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item')
+}
